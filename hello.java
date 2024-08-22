34de0c76e4192e346756d853258bb0a63177764d
Niladri <niladrisengit12@gmail.com>
2024-08-22 06:31:02 +0530

    first commit

---
+++ hello.java
@@ -0,0 +1,12 @@
+class Demo {
+    public static void main(String[] args) {
+        byte k = (byte) 257;
+        System.out.println(k);
+    }
+}
+
+// Implicit / Widening type casting in java
+// byte -> short -> char -> int -> long -> float -> double
+
+// Explicit / Narrowing type casting in java
+// double -> float -> long -> int -> char -> short -> byte
